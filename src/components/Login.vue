f1bfc52a3bdf58de11774c1ab0d0d377f60035a5
chenlei <chenlei@haixue.com>
2020-06-21 18:58:56 +0800

    新建

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
+</template>
+
+<!-- 注意不要随便留空格 -->
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.login_container{
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box{
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 10px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50px);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
+// path的前后注意要有空格，严格注意空格问题
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // path表示监听的地址
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
